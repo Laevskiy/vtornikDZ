727e5c931e8e5dae702494a04240f59f27872e58
Laevskiy <Laevskiy@mail.ru>
2025-01-13 20:56:39 +0300

    test 10.01

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,15 @@ const initState = {
     isLoading: false,
 }
 
+export type initStateType = typeof initState;
+
 export const loadingReducer = (state = initState, action: any): any => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+            case 'CHANGE_LOADING':
 
+                return {...state, isLoading:action.isLoading }
+        //
         default:
             return state
     }
